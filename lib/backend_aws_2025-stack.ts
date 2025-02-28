d7d8c1d3a4d9db5e3752e041f0a8a1e6124682f8
MaksimNK <maksmozir@gmail.com>
2025-02-28 23:05:07 +0300

    feat: add post labmda and update get lambda

--- lib/backend_aws_2025-stack.ts
+++ lib/backend_aws_2025-stack.ts
@@ -4,6 +4,7 @@ import * as nodejsLambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class BackendAws2025Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,22 +13,71 @@ export class BackendAws2025Stack extends cdk.Stack {
     const getProductsListLambda = new nodejsLambda.NodejsFunction(this, 'GetProductsListHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'handler',
-      entry: path.join(__dirname, 'lambdas/getProductsList.ts'), 
+      entry: path.join(__dirname, 'lambdas/getProductsList.ts'),
       bundling: {
         minify: true,
         sourceMap: true,
+      },
+      environment: {
+        PRODUCTS_TABLE: "products",
+        STOCKS_TABLE: "stocks"
       }
     });
 
     const getProductsByIdLambda = new nodejsLambda.NodejsFunction(this, 'GetProductsByIdHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'handler', 
+      handler: 'handler',
       entry: path.join(__dirname, 'lambdas/getProductById.ts'),
       bundling: {
         minify: true,
         sourceMap: true,
+      },
+      environment: {
+        PRODUCTS_TABLE: "products",
+        STOCKS_TABLE: "stocks"
       }
     });
+
+    const createProductLambda = new nodejsLambda.NodejsFunction(this, "CreateProductHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: path.join(__dirname, 'lambdas/createProduct.ts'),
+      bundling: {
+        minify: true,
+        sourceMap: true,
+      },
+      environment: {
+        PRODUCTS_TABLE: "products",
+        STOCKS_TABLE: "stocks"
+      }
+    });
+
+    createProductLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        "dynamodb:PutItem",
+        "dynamodb:TransactWriteItems"
+      ],
+      resources: [
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/products`,
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/stocks`
+      ]
+    }));
+
+    getProductsListLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ["dynamodb:Scan", "dynamodb:GetItem"],
+      resources: [
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/products`,
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/stocks`
+      ]
+    }));
+
+    getProductsByIdLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ["dynamodb:GetItem"],
+      resources: [
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/products`,
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/stocks`
+      ]
+    }));
 
     const api = new apigateway.RestApi(this, 'ProductsApi', {
       restApiName: 'Products Service',
@@ -42,11 +92,12 @@ export class BackendAws2025Stack extends cdk.Stack {
 
     const products = api.root.addResource('products');
     products.addMethod('GET', new apigateway.LambdaIntegration(getProductsListLambda));
+    products.addMethod('POST', new apigateway.LambdaIntegration(createProductLambda));
 
     const productById = products.addResource('{productId}');
     productById.addMethod('GET', new apigateway.LambdaIntegration(getProductsByIdLambda), {
       requestParameters: {
-        'method.request.path.id': true,
+        'method.request.path.productId': true,
       }
     });
   }
--- lib/lambdas/getProductsList.ts
+++ lib/lambdas/getProductsList.ts
@@ -1,14 +1,37 @@
-import {APIGatewayProxyResult } from "aws-lambda"
-import { formatResponse } from "./utils"
-import { products } from "./products"
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { formatResponse } from "./utils";
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const PRODUCTS_TABLE = process.env.PRODUCTS_TABLE;
+const STOCKS_TABLE = process.env.STOCKS_TABLE;
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        return formatResponse(200, products);
+        const productsData = await ddbDocClient.send(new ScanCommand({ TableName: PRODUCTS_TABLE }));
+        const stocksData = await ddbDocClient.send(new ScanCommand({ TableName: STOCKS_TABLE }));
+
+        const products = productsData.Items || [];
+        const stocks = stocksData.Items || [];
+
+        const joinedProducts = products.map((product) => {
+            const stock = stocks.find((s) => s.product_id === product.id);
+            return {
+                ...product,
+                count: stock ? stock.count : 0
+            };
+        });
+
+        return formatResponse(200, joinedProducts);
     } catch (error) {
+        console.error("Error fetching products:", error);
         return formatResponse(500, {
-            error: 'Internal Server Error',
-            message: 'An unexpected error occurred',
+            error: "Internal Server Error",
+            message: "An unexpected error occurred",
         });
     }
-}
+};
